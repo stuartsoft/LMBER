1a0a5c8d00affd9111db1e33a5a190acef065ecf
Stuart Bowman <stuartsoft2012@gmail.com>
2026-01-18 00:32:14 -0500

    Message line stretches across the screen

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { OLMessage } from './types/OLMessage';
 import { MessageLine } from './component/MessageLine';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [MatFormField, MatInput, MessageLine],
+  imports: [MatFormField, MatInput, MessageLine, MatToolbar, MatToolbarRow, MatButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
