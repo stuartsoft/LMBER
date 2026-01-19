9a452d037ea79fdcacc7fc76294e672087e89067
Stuart Bowman <stuartsoft2012@gmail.com>
2026-01-19 12:45:14 -0500

    interactions with the model!

--- src/app/services/message-broker.ts
+++ src/app/services/message-broker.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { OLMessage } from '../types/OLMessage';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageBroker {
-  public history : OLMessage[] = [
-  ]
-
+  httpClient = inject(HttpClient)
+  public history = signal([] as OLMessage[])
 
   sendMessage(content: string){
-    this.history.push(new OLMessage(content, "User"))
+    this.history.update(oldHistory => oldHistory.concat(new OLMessage(content, "User")))
+    let requestData = {
+      model: "gemma3",
+      messages: this.history().map((message) => { return {role: message.from, content: message.content}}),
+      stream: false
+    }
+
+    type ResponseShape =  {message: {content: string, role: string}}
+
+    this.httpClient.post('http://localhost:11434/api/chat', requestData).subscribe(response => {
+
+      let responseStuff = response as ResponseShape
+      let responseMessage = new OLMessage(responseStuff.message.content, responseStuff.message.role)
+      this.history.update(oldHistory => oldHistory.concat(responseMessage))
+      console.log(this.history())
+    })
+
   }
 }
