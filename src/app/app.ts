1460a85260f78590b0735886770c6165b09312db
Stuart Bowman <stuartsoft2012@gmail.com>
2026-01-18 22:19:15 -0500

    move message line to different folder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { MessageLine } from './component/MessageLine';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { MessageBroker } from './services/message-broker';
 import { SendMessageBar } from "./component/send-message-bar/send-message-bar";
 import { MatList } from '@angular/material/list';
+import { MessageLine } from './component/message-line/MessageLine';
 
 @Component({
   selector: 'app-root',
