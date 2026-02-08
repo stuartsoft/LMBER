dc1a14c08905c0b51b56ab2585093ccaf4aa1292
Stuart Bowman <stuartsoft2012@gmail.com>
2026-02-08 17:33:14 -0500

    Streaming responses! Also analytics off

--- src/app/services/message-broker.ts
+++ src/app/services/message-broker.ts
@@ -9,23 +9,47 @@ export class MessageBroker {
   httpClient = inject(HttpClient)
   public history = signal([] as OLMessage[])
 
-  sendMessage(content: string){
+  sendMessage(content: string) {
     this.history.update(oldHistory => oldHistory.concat(new OLMessage(content, "User")))
     let requestData = {
       model: "gemma3",
-      messages: this.history().map((message) => { return {role: message.from, content: message.content}}),
-      stream: false
+      messages: this.history().map((message) => { return { role: message.from, content: message.content } }),
+      stream: true
     }
 
-    type ResponseShape =  {message: {content: string, role: string}}
+    type ResponseShape = { message: { content: string, role: string } }
 
-    this.httpClient.post('http://localhost:11434/api/chat', requestData).subscribe(response => {
+    // this.httpClient.post('http://localhost:11434/api/chat', requestData).subscribe(response => {
 
-      let responseStuff = response as ResponseShape
-      let responseMessage = new OLMessage(responseStuff.message.content, responseStuff.message.role)
-      this.history.update(oldHistory => oldHistory.concat(responseMessage))
-      console.log(this.history())
+    //   let responseStuff = response as ResponseShape
+    //   let responseMessage = new OLMessage(responseStuff.message.content, responseStuff.message.role)
+    //   this.history.update(oldHistory => oldHistory.concat(responseMessage))
+    //   console.log(this.history())
+    // })
+
+    this.history.update(oldHistory => oldHistory.concat(new OLMessage("", "assistant")))
+    fetch('http://localhost:11434/api/chat', { method: "POST", body: JSON.stringify(requestData) }).then(response => {
+      let rs = response.body as ReadableStream
+      let reader = rs.getReader()
+      reader.read().then(rsrr => this.readReaderUntilDone(rsrr, reader))
     })
+  }
 
+  readReaderUntilDone(chunk: ReadableStreamReadResult<any>, reader: ReadableStreamDefaultReader) {
+    if (chunk.done){
+      return
+    }
+    let nextMessagePiece: { message: { role: string, content: string } } = this.hydrateChunk(chunk)
+    this.history.update((oldHistory: OLMessage[]) => {
+      let lastmessage = oldHistory[oldHistory.length - 1]
+      lastmessage.content += nextMessagePiece.message.content
+      return [...oldHistory]
+    })
+    reader.read().then(rsrr => this.readReaderUntilDone(rsrr, reader))
+
+  }
+
+  hydrateChunk(chunk: ReadableStreamReadResult<any>): any {
+    return JSON.parse(new TextDecoder().decode(chunk.value))
   }
 }
